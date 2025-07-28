cdcbf160c53b248687ace48c222e41bcfbea45ef
LoicBrassart <loic.brassart@gmail.com>
2025-07-28 09:23:16 +0200

    Add other possible solution for A2

--- algos/src/challenges/A2/answer.ts
+++ algos/src/challenges/A2/answer.ts
@@ -7,20 +7,37 @@
  * @param categories List of categories without tags, but with ads
  * @returns List of categories with a new prop tags
  */
-
 // ↓ uncomment bellow lines and add your response!
 export default function ({
   categories,
 }: {
   categories: Category[];
 }): CategoryWithTags[] {
+  //pr chq categorie
   return categories.map((category) => {
-    const unSortedTags = category.ads.reduce((acc, ad) => {
-      return [...acc, ...ad.tags];
-    }, [] as string[]);
-    const categoryTags = [...new Set(unSortedTags.sort())];
-    return { ...category, tags: categoryTags };
+    // je cree une liste de tags viode
+    let allTags: string[] = [];
+    // j'ajoute une liste de tags triés et uniques
+    // pour chaque annonce
+    category.ads.map((ad) => {
+      // je regarde tous les tags existants dans l'annonce: ad.tags
+      // ajouter les tags à allTags
+      const unorderedTags = ad.tags;
+      allTags.push(...unorderedTags);
+      //return new Set(unorderedTags)
+    });
+
+    return { ...category, tags: [...new Set(allTags.sort())] };
   });
+  // ne pas canger l'ordre
+
+  // return categories.map((category) => {
+  //   const unSortedTags = category.ads.reduce((acc, ad) => {
+  //     return [...acc, ...ad.tags];
+  //   }, [] as string[]);
+  //   const categoryTags = [...new Set(unSortedTags.sort())];
+  //   return { ...category, tags: categoryTags };
+  // });
 }
 
 // used interfaces, do not touch
