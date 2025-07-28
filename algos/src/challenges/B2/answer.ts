f21ca8aca9d9801f301a2b316184398a2f368b16
LoicBrassart <loic.brassart@gmail.com>
2025-07-28 11:26:59 +0200

    Add solution for B2

--- algos/src/challenges/B2/answer.ts
+++ algos/src/challenges/B2/answer.ts
@@ -1,6 +1,6 @@
 /**
  * In this challenge, you have to get all the images sent in a conversation (list of messages).
- * Images must be sorted by message datetime they are attached to (recents first, if datetimes are the same, should be sorted by content length).
+ * Images must be sorted by message datetime they are attached to (oldest first, if datetimes are the same, should be sorted by content length (shortest first)).
  * You should not display duplicates. If duplicates are found, the recent one should be kept.
  * This algo is useful to create a medias gallery in a conversation app (such as in WhatsApp conversations)
  *
@@ -9,15 +9,31 @@
  */
 
 // ↓ uncomment bellow lines and add your response!
-/* 
 export default function ({
   messages,
 }: {
   messages: MessageWithImages[];
 }): string[] {
-  return []
+  // (NB: "..." === "le contenu de")
+  const images = messages
+    // trier messages par ordre d'envoi / lgueur message [B1]
+    .sort((msgA, msgB) => {
+      const diff = msgA.sentAt.localeCompare(msgB.sentAt);
+      if (diff) return diff;
+      return msgA.content.length - msgB.content.length;
+    })
+    // créer array acc [A2 ?]
+    // mapper sur messages
+    .reduce((acc, message) => {
+      // acc.push (<- faites pas ça) ou acc = [...acc, ...images]
+      return [...acc, ...message.images];
+    }, [] as string[]);
+
+  // dedoublonner: créer un Set à partir d'acc, puis le retransformer en tableau [A2]
+  const deduped = [...new Set(images)];
+  // return
+  return deduped;
 }
- */
 
 // used interfaces, do not touch
 export interface MessageWithImages {
